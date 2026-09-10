faaa1be9c831517c81f486efdb511d74bae448d6
Anikesh_Ghosh123 <anikeshghosh935@gmail.com>
2026-09-11 00:27:14 +0530

    remove unnecessary frontend url from allow origin

--- backend-MindVarta/app.py
+++ backend-MindVarta/app.py
@@ -76,12 +76,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:3000",
-        "http://localhost:4173",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
-        "https://mindvarta-mental-health-assistant.onrender.com",
         "https://mindvarta-mental-health-assistant-2.onrender.com",
     ],
     allow_credentials=True,
